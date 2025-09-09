ff80e0d5d62696a44dec58af16959fd7c36a93ef
nastushal1102-oss <nastushal1102@gmail.com>
2025-09-09 17:29:48 +0300

    Add files via upload

---
+++ LZ1_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x;
+	cout << "Введіть x: ";
+	cin >> x;
+
+	if (2 * x + 1 == 0 || 1 + 3 * x == 0) {
+		cout << "Помилка: ділення на нуль!" << endl;
+		return 1;
+	}
+	double y = (sin(x) - x * x / (2 * x + 1)) + ((1 + x) * (1 + x)) / (1 + 3 * x);
+	cout << "y = " << y << endl;
+	return 0;
+}
